7179b08a5e4c3b2b202d747f3f2c97286d340c38
Debora <deboribo@gmail.com>
2021-05-13 14:03:12 -0700

    finished pseudocode

--- fruit_pies.py
+++ fruit_pies.py
@@ -5,3 +5,10 @@
 Determin the price of a specific pie
 Each type of fruit pie has a different price
 """
+
+"""
+obtain user input
+provide display options to user
+crate a dictionary with the types of fruits and prices
+display results to user
+"""
